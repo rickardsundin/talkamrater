6264ed410bbd7522fe7ea06dc7e97c3eaa66a124
Rickard Sundin <rickard@snart.nu>
2017-01-06 11:58:21 +0100

    Add cljsbuild to build releases

--- project.clj
+++ project.clj
@@ -1,7 +1,15 @@
 (defproject talkompisar "0.1.0-SNAPSHOT"
   :description "Talkompisar!"
+  :plugins [[lein-cljsbuild "1.1.5"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.omcljs/om "1.0.0-alpha34"]
                  [racehub/om-bootstrap "0.6.1"]
-                 [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
+                 [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]]
+  :cljsbuild {
+              :builds [{:id "release"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :output-to "main.js"
+                                   :optimizations :advanced
+                                   :pretty-print false}}]})
